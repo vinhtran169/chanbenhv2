11e70d7e25507473458ce61b780554cd981246b0
NGUYEN QUOC THINH <q_thinh@brycen-vn.local>
2018-01-20 11:19:07 +0700

    Thinh - add image blob data to content - 20180120

--- MyComponents/ChiTietBenh.js
+++ MyComponents/ChiTietBenh.js
@@ -7,38 +7,83 @@ import {
   Image, 
   WebView } from 'react-native';
 import Globals from '../globals';
+import Dimensions from 'Dimensions';
+import * as Progress from 'react-native-progress';
 
 var styles = require('../styles/main');
-var propsData;
+var propsData = [];
+var imageUri = '';
+var imgTag = '';
+var contentHtml = '';
 
 export default class ChiTietBenh extends Component {
   constructor(props){
     super(props);
+
+    Globals.DetectStringFormat();
     propsData = props.route.props.dataSend;
+
+    this.state = { loadScreen: false }
   }
 
-  // Go Screen
-  gotoPage(componentName, screenName) {
-    this.props.navigator.push({ component: componentName, name: screenName });
+  // Call before render
+  componentWillMount(){
+    this.resizeImage();
+  }
+
+  //Set image content
+  resizeImage(){
+    let totalPaddingContainer = 40;
+    let screenWidth = Dimensions.get('window').width - totalPaddingContainer;
+    imageUri = (propsData.HINHANHMOTA != '' && propsData.HINHANHMOTA != null)? 'data:image/png;base64,' + propsData.HINHANHMOTA : '';
+
+    contentHtml = propsData.MOTACHITIET;
+    if (imageUri == ''){
+      this.setState({ loadScreen: true });
+      contentHtml = contentHtml.replace('ReplaceImageDetailContent', '');
+    }
+    else{
+      Image.getSize(imageUri, (width, height) => {
+        if (width > screenWidth){
+          imgTag = '<div style="text-align:center;"><img src="{0}" width="{1}" /></div>'.format(imageUri, screenWidth.toString());
+        }
+        else if (width > screenWidth * 70/100){
+          imgTag = '<div style="text-align:center;"><img src="{0}" width="{1}" /></div>'.format(imageUri, width);
+        }
+        else{
+          imgTag = '<img src="{0}" width="{1}" align="left" />'.format(imageUri, width);
+        }
+
+        contentHtml = imgTag + contentHtml;
+
+        this.setState({ loadScreen: true });
+        this.render();
+      });
+    }
   }
 
   // Render main
   render() {
-    return (
-      <View style={styles.detailContainer}>
-        <View style={styles.detailTitleContainer}>
-          <Text style={styles.detailTitle}>{propsData.TENBENH.toUpperCase()}</Text>
+    if (!this.state.loadScreen){
+      return (
+        <View style={styles.containerProgress}>
+          <Progress.CircleSnail style={styles.circleProgress} thickness={6} size={100} />
         </View>
+      );
+    }
+    else {
+      return (
+        <View style={styles.detailContainer}>
+          <View style={styles.detailTitleContainer}>
+            <Text style={styles.detailTitle}>{propsData.TENBENH.toUpperCase()}</Text>
+          </View>
 
-        <View style={styles.detailImageContainer}>
-          <Image source={require('../images/doctor.png')} style={styles.detailImage}></Image>
+          <WebView source={{html: contentHtml}} 
+            style={styles.detailContentContainer}
+            startInLoadingState={true}
+            scalesPageToFit={true} />
         </View>
-
-        <WebView source={{html: propsData.MOTACHITIET}} 
-          style={styles.detailContentContainer}
-          startInLoadingState={true}
-          scalesPageToFit={true} />
-      </View>
-    );
+      );
+    }
   }
 }
--- globals.js
+++ globals.js
@@ -10,6 +10,21 @@ class Globals{
   static ScrMeoVat = 'MẸO VẶT';
   static ScrMeoVatChiTiet = 'CHI TIẾT MẸO VẶT';
 
+  static DetectStringFormat(){
+    // First, checks if it isn't implemented yet.
+    if (!String.prototype.format) {
+      String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+          return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+          ;
+        });
+      };
+    }
+  }
+
 
   //=======START Sqlite3==================
   static db = null;
